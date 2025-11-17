5757a421ac3218aba705f45171ee88b46cdb682d
citrusarc <citrusarc.studio@gmail.com>
2025-11-17 11:40:43 +0800

    add particles bacgrkound

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "@/app/globals.css";
+import { SparklesCore } from "@/components/ui/shadcn-io/sparkles";
 
 import SplashScreen from "@/components/ui/SplashScreen";
 
@@ -17,9 +18,20 @@ export default function RootLayout({
     <html lang="en">
       <body
         suppressHydrationWarning
-        className="relative sm:h-screen sm:overflow-hidden  overflow-x-hidden overflow-y-auto min-h-screen max-w-full antialiased bg-[linear-gradient(to_bottom_right,#242615,#2E2D19,#342814,#3A2312,#1F1812,#2B2623)]"
+        className="relative sm:h-screen sm:overflow-hidden overflow-x-hidden overflow-y-auto min-h-screen max-w-full antialiased bg-[linear-gradient(to_bottom_right,#242615,#2E2D19,#342814,#3A2312,#1F1812,#2B2623)]"
       >
         <SplashScreen />
+        <div className="absolute inset-0 -z-10">
+          <SparklesCore
+            background="transparent"
+            minSize={0.6}
+            maxSize={1.4}
+            particleDensity={10}
+            className="w-full h-full"
+            particleColor="#FFFFFF"
+            speed={1}
+          />
+        </div>
         <main>{children}</main>
       </body>
     </html>
